eccb31b58297a253421a52f96ff8bd34b629143a
ashutoshkumar1020 <115533717+ashutoshkumar1020@users.noreply.github.com>
2023-02-01 23:23:00 +0530

    iwquestions

---
+++ iwsession.js
@@ -0,0 +1,13 @@
+Q.1)
+console.log("Masai School");
+console.log("A Transformation in Education");
+
+Q.2)
+let name = "Ashutosh Kumar";
+console.log(name);
+
+name = "Diwakar Upadhyay";
+console.log(name);
+
+name = "Mala Devi";
+console.log(name);
